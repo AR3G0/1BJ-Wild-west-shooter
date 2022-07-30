87d445b725ae1a77131c159a98236dd2846c275e
AR3G0 <noah109carrico@gmail.com>
2022-07-30 13:02:18 -0500

    Music is live

--- 1BJ-wild-west-shooter/Assets/musicControler.cs
+++ 1BJ-wild-west-shooter/Assets/musicControler.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class musicControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] music;
+    private AudioSource audioSource;
+    private GameManager gameManagerScript;
+
+    private void Awake()
     {
-        
+        gameManagerScript = GetComponent<GameManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // in the main menu loop the intro sound
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MainMenu"))
+        {
+
+            if (!audioSource.isPlaying || audioSource.clip == music[2])
+            {
+                audioSource.clip = music[0];
+                audioSource.Play(0);
+            }
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Gameplay"))
+        {
+            if (audioSource.clip == music[0])
+            {
+                audioSource.Stop();
+                audioSource.volume = 0.08f;
+                audioSource.clip = music[1];
+                audioSource.Play(0);
+            }
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("GameOver"))
+        {
+            if (audioSource.clip == music[1])
+            {
+                audioSource.Stop();
+                audioSource.volume = 0.45f;
+                audioSource.clip = music[2];
+                audioSource.Play(0);
+            }
+        }
     }
 }
